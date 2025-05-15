a381cdb4c3d05a065cfa85b7c0a8eddbc6d93f9d
LISis3 <97257031+BronzeCode@users.noreply.github.com>
2025-05-16 02:58:25 +0500

    Added configuration logic to srvt.java

--- src/main/java/com/gfs/srvtool/srvt.java
+++ src/main/java/com/gfs/srvtool/srvt.java
@@ -1,11 +1,21 @@
 package com.gfs.srvtool.Main.Main;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     @override
     public void onEnable() {
         getLogger().info("Thanks for using srvtool from GFS! Our tg channel: @GlowingFoxStudios ; our discord: ");
-        getCommand("help").setExecutor(helpExecutor());
+
+saveDefaultConfig();
+getConfig().options().copyDefaults(true);
+saveConfig();
+       
+getConfig().set("npcs.id.1.name", "Biba");
+saveConfig():
+ getCommand("help").setExecutor(helpExecutor());
     }
     @override
     public void onDisable {
